43d93d82dfaadfd58fb82ac2b10e5bb8a8bc9b17
BraidaFac <facundobraida9@gmail.com>
2023-07-19 10:49:25 -0300

    Change domain and clientID Auth0

--- serviYa/src/auth_config.ts
+++ serviYa/src/auth_config.ts
@@ -1,6 +1,6 @@
 const config = {
-  domain: "dev-4hpsoaozhjeg0ij7.us.auth0.com",
-  clientId: "QVCWTgacyPUJMlk5PyTyOfrsHBZnLaC4",
+  domain: "dev-08zsbegr2fxkuoh6.us.auth0.com",
+  clientId: "WSuxo9tS7hKRcoJSogmxoUqIwcmzdx9y",
 };
 
 export default config;
