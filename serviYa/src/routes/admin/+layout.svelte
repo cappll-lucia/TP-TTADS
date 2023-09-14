e88d85e41091c2de155487b961ea68785007b720
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-09-14 19:01:54 -0300

    navbar in dashboard

---
+++ serviYa/src/routes/admin/+layout.svelte
@@ -0,0 +1,25 @@
+<nav>
+	<ul>
+		<li><a href="/admin/service">Services</a></li>
+		<li><a href="/admin/city">Cities</a></li>
+		<li><a href="/admin/province">Provinces</a></li>
+	</ul>
+</nav>
+
+<slot />
+
+<style lang="scss">
+	ul {
+		display: flex;
+		gap: 1rem;
+	}
+	nav {
+		margin-right: 2rem;
+		margin-left: 2rem;
+		padding-left: 6rem;
+		border: 1px solid gray;
+		border-top: none;
+		border-radius: 0 0 5rem 5rem;
+		text-align: center;
+	}
+</style>
