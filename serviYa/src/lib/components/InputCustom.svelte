3c24d3986271f415ac07f1163bfc3a33eeb4f949
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-09-17 19:22:16 -0300

    input compoenet

---
+++ serviYa/src/lib/components/InputCustom.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+	import { derived, type Writable } from 'svelte/store';
+	import { fly } from 'svelte/transition';
+
+	type Form =
+		| {
+				data: { [x: string]: string };
+				errors: {
+					[x: string]: string[] | undefined;
+					[x: number]: string[] | undefined;
+					[x: symbol]: string[] | undefined;
+				};
+				message?: undefined;
+		  }
+		| { message: string; data?: undefined; errors?: undefined }
+		| null;
+
+	export let formStore: Writable<Form>;
+	export let name: string;
+	export let type: 'text' | 'password' | 'email';
+	export let placeholder: string;
+
+	const hasError = derived(formStore, (form) => Boolean(form && form.errors && form.errors[name]));
+	const value = derived(formStore, (form) => (form && form.data ? form.data[name] : ''));
+	const error = derived(formStore, (form) =>
+		form && form.errors && form.errors[name] ? form.errors[name] : ''
+	);
+</script>
+
+<input
+	{name}
+	{type}
+	{placeholder}
+	aria-label={name}
+	class={$hasError ? 'input-error' : 'input'}
+	value={$value}
+/>
+{#if $hasError}
+	<span
+		in:fly={{ y: -30, x: -2 }}
+		class="error">{$error}</span
+	>
+{/if}
+
+<style>
+	.error {
+		font-size: medium;
+		color: red;
+		position: relative;
+		display: inline-block;
+		height: 2rem;
+		margin: 0;
+	}
+
+	.input-error {
+		--spacing: 0;
+		border: red solid 2px;
+		margin-bottom: 0rem;
+	}
+</style>
--- serviYa/src/routes/register/+page.svelte
+++ serviYa/src/routes/register/+page.svelte
@@ -1,174 +1,139 @@
 <script lang="ts">
-  import {registerSchema} from "./registerSchema"
-  import {enhance} from '$app/forms'
-  import type { ActionData } from "./$types";
-    import { ZodError } from "zod";
-  export let form;
-  let loading=false;
-  let a=0
+	import { registerSchema } from './registerSchema';
+	import { enhance } from '$app/forms';
+	import type { ActionData } from './$types';
+	import { ZodError } from 'zod';
+	import InputCustom from '$lib/components/InputCustom.svelte';
+	import { writable } from 'svelte/store';
 
+	export let form;
+	const formStore = writable(form);
+	$: formStore.set(form);
 
-  function validateOrThrow(formdata:FormData) {
-    const obj={} as any;
-    formdata.forEach((v:any,k:any)=>obj[k]=v)
-    registerSchema.parse(obj)
-  }
+	let loading = false;
 
+	function validateOrThrow(formdata: FormData) {
+		const obj = {} as any;
+		formdata.forEach((v: any, k: any) => (obj[k] = v));
+		registerSchema.parse(obj);
+	}
 
-
-  function manageError(error:any){
-    if (error instanceof ZodError) {
-      const { fieldErrors } = error.flatten();
-      form={errors:fieldErrors} as ActionData
-    }
-  }
-
+	function manageError(error: any) {
+		if (error instanceof ZodError) {
+			const { fieldErrors } = error.flatten();
+			form = { errors: fieldErrors } as ActionData;
+		}
+	}
 </script>
 
 <main class="container">
-  <article class="grid">
-    <div class="form-panel">
-      <hgroup>
-        <h1>Registro</h1>
-      </hgroup>
-      <form method="POST" class="register-form" use:enhance={
-        ({formData})=>{
-          try{
-            validateOrThrow(formData)
-            loading=true;
-            return ({update} )=>{
-              loading=false
-              update()
-            }
-          }
-          catch (error){
-            manageError(error)
-          }
-        }
-        }>
-        <div class="inputs">
-          <input
-          type="text"
-          name="email"
-          placeholder="Email"
-          aria-label="Email"
-          autocomplete="email"
-          value={form?.data?.email ?? ''}
-          class="{form?.errors?.email ? 'input-error' : 'input'}"
-          />
-          {#if form?.errors?.email}
-          <span class="error">{form?.errors?.email[0]}</span>
-          {/if}
-          <input
-          type="text"
-          name="name"
-          placeholder="Name"
-          aria-label="Name"
-          class="{form?.errors?.name ? 'input-error' : 'input'}"
-          value={form?.data?.name ?? ''}
-          />
-          {#if form?.errors?.name}
-          <span class="error">{form?.errors?.name[0]}</span>
-          {/if}
-          <input
-          type="password"
-          name="password"
-          placeholder="Password"
-          aria-label="Password"
-          autocomplete="current-password"
-          class="{form?.errors?.password ? 'input-error' : 'input'}"
-          value={form?.data?.password ?? ''}
-          />
-          {#if form?.errors?.password}
-          <span class="error">{form?.errors?.password[0]}</span>
-          {/if}
-          <input
-          type="password"
-          name="confirmPassword"
-          placeholder="Confirm Password"
-          aria-label="confirm Password"
-          value={form?.data?.confirmPassword ?? ''}
-          class="{form?.errors?.confirmPassword ? 'input-error' : 'input'} {form?.message ? 'message-error' : ''}"
-          />
-          {#if form?.errors?.confirmPassword}
-          <span class="error">{form?.errors?.confirmPassword[0]}</span>
-          {/if}
-          {#if form?.message}
-          <span class="error">{form?.message}</span>
-          {/if}
-        </div>
-        <button type="submit" class="contrast submit-btn" typeof="submit" aria-busy={loading}>Register</button>
-      </form>
-    </div>
-    <div id="register-img" />
-  </article>
+	<article class="grid">
+		<div class="form-panel">
+			<hgroup>
+				<h1>Registro</h1>
+			</hgroup>
+			<form
+				method="POST"
+				class="register-form"
+				use:enhance={({ formData }) => {
+					try {
+						validateOrThrow(formData);
+						loading = true;
+						return ({ update }) => {
+							loading = false;
+							update();
+						};
+					} catch (error) {
+						manageError(error);
+					}
+				}}
+			>
+				<div class="inputs">
+					<InputCustom
+						{formStore}
+						name="email"
+						type="email"
+						placeholder="Email"
+					/>
+					<InputCustom
+						{formStore}
+						name="name"
+						type="text"
+						placeholder="Name"
+					/>
+					<InputCustom
+						{formStore}
+						name="password"
+						type="password"
+						placeholder="Password"
+					/>
+					<InputCustom
+						{formStore}
+						name="confirmPassword"
+						type="password"
+						placeholder="Confirm Password"
+					/>
+					{#if form?.message}
+						<span class="error">{form?.message}</span>
+					{/if}
+				</div>
+				<button
+					type="submit"
+					class="contrast submit-btn"
+					typeof="submit"
+					aria-busy={loading}>Register</button
+				>
+			</form>
+		</div>
+		<div id="register-img" />
+	</article>
 </main>
 
 <style lang="scss">
-.container{
-  display:flex;
-  height: 40rem;
-  justify-content:center;
-  padding: 2rem 0;
-  align-items:flex-start;
-  z-index:0;
-  .grid{
-    .form-panel{
-      display:flex;
-      justify-content: space-between;
-      align-items: space-between;
-      flex-direction:column;
-      height:100%;
-      h1{
-        font-size:2rem;
-      }
-    }
-  }
-}
-.mt-1{
-  margin-top:1rem;  
-}
-#register-img {
-    background-image: url('../../ai.profClient.png');
-    background-position: center;
-    background-size:cover;
-  }
-.register-form{
-  --form-element-spacing-vertical: 0.5rem;
-      display:flex;
-      justify-content: space-between;
-      flex-direction:column;
-      margin-bottom:0;
-      .inputs{
-        height:25rem;
-        .input{
-          display:inline-block;
-          --spacing:0;
-          margin-bottom:2rem;
-        }
-        .input:first-child{
-          margin-top:1rem;
-        }
-        .message-error{
-          margin-bottom:1rem;
-        }
-      }
-    .submit-btn{
-      margin-bottom:0;
-    }
-}
-.error{
-  font-size: small;
-  color: red;
-  position: relative;
-  display:inline-block;
-  height:2rem;
-  margin:0;
-}
-.input-error{
-  --spacing:0;
-  border: red solid 2px;
-  margin-bottom:0rem;
-}
-
+	.container {
+		display: flex;
+		height: 40rem;
+		justify-content: center;
+		padding: 2rem 0;
+		align-items: flex-start;
+		z-index: 0;
+		.grid {
+			.form-panel {
+				display: flex;
+				justify-content: space-between;
+				align-items: space-between;
+				flex-direction: column;
+				height: 100%;
+				h1 {
+					font-size: 2rem;
+				}
+			}
+		}
+	}
+	#register-img {
+		background-image: url('../../ai.profClient.png');
+		background-position: center;
+		background-size: cover;
+	}
+	.register-form {
+		--form-element-spacing-vertical: 0.5rem;
+		display: flex;
+		justify-content: space-between;
+		flex-direction: column;
+		margin-bottom: 0;
+		.inputs {
+			width: 80vw;
+		}
+		.submit-btn {
+			margin-bottom: 0;
+		}
+	}
+	.error {
+		font-size: small;
+		color: red;
+		position: relative;
+		display: inline-block;
+		height: 2rem;
+		margin: 0;
+	}
 </style>
