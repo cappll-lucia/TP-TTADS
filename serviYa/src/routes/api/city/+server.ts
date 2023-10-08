494f96a80c139741ca4c83013cb3d13d1dc013f2
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-10-08 20:25:41 -0300

    city endpoint

---
+++ serviYa/src/routes/api/city/+server.ts
@@ -0,0 +1,26 @@
+import { prisma } from '$lib/server/lucia/prisma';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async ({ url }) => {
+	const search = url.searchParams.get('name');
+
+	if (!search || search.length < 2) {
+		const errorBody = { error: 'name must be at least 2 characters long' };
+		return new Response(JSON.stringify(errorBody), {
+			status: 400,
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+	}
+
+	const arr = await prisma.city.findMany({
+		where: { name: { contains: search, mode: 'insensitive' } }
+	});
+	return new Response(JSON.stringify({ arr }), {
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+};
