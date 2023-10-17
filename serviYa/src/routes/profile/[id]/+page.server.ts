65067d5106853cae353484c1cc644ff0d42c8204
BrunoMollo <dia.mollo.bruno@gmail.com>
2023-10-17 20:25:58 -0300

    add profesional profile skeleton

--- serviYa/src/routes/profile/[id]/+page.server.ts
+++ serviYa/src/routes/profile/[id]/+page.server.ts
@@ -3,6 +3,13 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params }) => {
 	const profesional_id = params.id;
+	const profesional = await prisma.authUser.findUnique({
+		where: { id: profesional_id },
+		include: { services: true }
+	});
+	if (!profesional) {
+		throw Error('Profesional not found');
+	}
 
-	return { res };
+	return { profesional };
 };
--- serviYa/src/routes/profile/[id]/+page.svelte
+++ serviYa/src/routes/profile/[id]/+page.svelte
@@ -1,6 +1,49 @@
-<script>
-	export let data;
+<script lang="ts">
+	import type { PageData } from './$types';
+	export let data: PageData;
 	$: console.log(data);
+	const { profesional } = data;
 </script>
 
-udahsafsaonnfasui
+<main class="container">
+	<article>
+		<h1>{profesional.name}</h1>
+		<h2>{profesional.services[0].name}</h2>
+		<p>{profesional.description}</p>
+	</article>
+
+	<article class="turns">
+		<table>
+			<tr>
+				<th>Lunes</th>
+				<th>Martes</th>
+				<th>Miercoles</th>
+				<th>Jueves</th>
+				<th>Viernes</th>
+				<th>Sabado</th>
+			</tr>
+			<tr>
+				<td><button class="agend">Agendar</button></td>
+				<td><button class="agend">Agendar</button></td>
+				<td><button class="agend">Agendar</button></td>
+				<td><button class="agend">Agendar</button></td>
+				<td><button class="agend">Agendar</button></td>
+				<td><button class="agend">Agendar</button></td>
+			</tr>
+		</table>
+	</article>
+</main>
+
+<style>
+	.turns {
+		overflow-x: scroll;
+	}
+	th {
+		text-align: center;
+	}
+	.agend {
+		text-align: center;
+		padding: 0.4rem;
+		font-size: 0.98rem;
+	}
+</style>
