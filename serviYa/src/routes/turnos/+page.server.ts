ce880ecca911d20b288177160a457ea780df5863
Ramiro Di Giacinti <71228261+Ramiro-DG@users.noreply.github.com>
2023-10-19 19:33:46 -0300

    appointment menu improvements

--- serviYa/src/routes/turnos/+page.server.ts
+++ serviYa/src/routes/turnos/+page.server.ts
@@ -1,6 +1,6 @@
 import { prisma } from '$lib/server/lucia/prisma';
 import { redirect } from '@sveltejs/kit';
-import type { PageServerLoad } from './$types';
+import type { PageServerLoad, Actions } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const { user } = await locals.auth.validateUser();
@@ -10,5 +10,25 @@ export const load: PageServerLoad = async ({ locals }) => {
 	const appointments = await prisma.appointment.findMany({
 		where: { profesional_id: user.userId }
 	});
-	return { appointments };
+	const pendingAppointments = appointments.filter(
+		(appointment) => appointment.state === 'UNCONFIRMED'
+	);
+	const confirmedAppointments = appointments.filter((appointment) => appointment.state === 'TO_DO');
+	const otherAppointments = appointments.filter(
+		(appointment) => appointment.state === 'DONE' || appointment.state === 'REJECTED'
+	);
+	return { pendingAppointments, confirmedAppointments, otherAppointments };
+};
+
+export const actions: Actions = {
+	confirmar: async ({ request }) => {
+		const data = await request.formData();
+		const appointment_id = data.get('appointment_id')?.toString();
+		//todo falta validar cosas (segun bruno)
+		await prisma.appointment.update({
+			where: { id: appointment_id },
+			data: { state: 'TO_DO' }
+		});
+		return { status: 200 };
+	}
 };
--- serviYa/src/routes/turnos/+page.svelte
+++ serviYa/src/routes/turnos/+page.svelte
@@ -1,8 +1,99 @@
 <script lang="ts">
-	export let data;
-	console.log('::::', data.appointments);
+	import type { PageData } from './$types';
+	export let data: PageData;
+
+	const { confirmedAppointments, otherAppointments, pendingAppointments } = data;
+
+	let modal: HTMLElement;
+	let appointment_data = { id: '', description: '' };
+
+	const closeModal = () => {
+		modal.attributes.removeNamedItem('open');
+		appointment_data.description = '';
+	};
+
+	const setUpModal = (app: { id: string; description: string }) => {
+		appointment_data.description = app.description;
+		appointment_data.id = app.id;
+		modal.setAttribute('open', 'true');
+	};
 </script>
 
-<pre>
-  {JSON.stringify(data)}
-</pre>
+<dialog
+	id="modal"
+	bind:this={modal}
+>
+	<article>
+		<form method="post">
+			<header>
+				<a
+					href="#"
+					on:click={closeModal}><i class="mi-close" /></a
+				>
+				<h3>Detalles del Turno</h3>
+			</header>
+			<input
+				name="appointment_id"
+				value={appointment_data?.id || ''}
+				hidden
+			/>
+			<p>{appointment_data?.description || ''}</p>
+			<footer>
+				<button
+					class="cancel outline"
+					type="submit"
+					formaction="?/cancelar"
+				>
+					Cancelar
+				</button>
+				<button
+					type="submit"
+					class="accept"
+					formaction="?/confirmar"
+				>
+					Aceptar
+				</button>
+			</footer>
+		</form>
+	</article>
+</dialog>
+
+<main class="container">
+	<h1>Turnos</h1>
+	<article>
+		<h2>Pendientes de confirmacion</h2>
+		{#each pendingAppointments as app}
+			<div>
+				<h3>{app.date}</h3>
+				<p>{app.description}</p>
+				<button on:click={() => setUpModal(app)}>Detalles</button>
+			</div>
+		{/each}
+	</article>
+	<article>
+		<h2>Por realizar</h2>
+		{#each confirmedAppointments as app}
+			<div>
+				<h3>{app.date}</h3>
+				<p>{app.description}</p>
+			</div>
+		{/each}
+	</article>
+	<article>
+		<h2>Historial</h2>
+		{#each otherAppointments as app}
+			<div>
+				<h3>{app.date}</h3>
+				<p>{app.description}</p>
+				<p>{app.state}</p>
+			</div>
+		{/each}
+	</article>
+</main>
+
+<style>
+	.cancel {
+		border-color: red;
+		color: red;
+	}
+</style>
