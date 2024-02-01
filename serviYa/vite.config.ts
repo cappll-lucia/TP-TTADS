3e235ac5e2b50f2c495ce84a6f4853977e8397ea
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-02-01 15:45:50 -0300

    fix playwritght

--- serviYa/vite.config.ts
+++ serviYa/vite.config.ts
@@ -1,7 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	server: { port: 3000 }
+	server: { port: 3000 },
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
 });
