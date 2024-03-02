09207ce56f572810b223771b31e031ca90e665a8
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-03-01 22:48:25 -0300

    Aca no paso nada si meca dice algo de la autenticacion comentada, enviarle este commit :)

--- serviYa/src/hooks.server.ts
+++ serviYa/src/hooks.server.ts
@@ -1,8 +1,14 @@
 // hooks.server.ts
 import { auth } from '$lib/server/lucia/lucia';
-import type { Handle } from '@sveltejs/kit';
+import { redirect, type Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.locals.auth = auth.handleRequest(event);
+	if (event.url.pathname.includes('admin')) {
+		const { user, session } = await event.locals.auth.validateUser();
+		if (!session || user.role !== 'ADMIN') {
+			throw redirect(302, '/');
+		}
+	}
 	return await resolve(event);
 };
